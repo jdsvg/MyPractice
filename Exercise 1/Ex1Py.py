35c3db138dd51cb813e8b8dd380800f016b1b503
John Villamil <52754373+jdsvg@users.noreply.github.com>
2023-01-04 02:32:06 +0000

    Continuing 03012023

--- Exercise 1/Ex1Py.py
+++ Exercise 1/Ex1Py.py
@@ -1,23 +1,17 @@
-def val_ai(num):
+def val(num, pross):
   try: num = int(num)
   except: print("only int numbers"); quit()
-  if num < 0 or num>1000: print("Numero demasiado grande. Empieza de nuevo"); quit()
+  if pross == 1:
+    if num < 0 or num>50: print("Numero demasiado grande. Empieza de nuevo"); quit()
+  if pross == 2:
+    if num < 0 or num>1000: print("Numero demasiado grande. Empieza de nuevo"); quit()
   return num
-
-
+#
 arr = []
-num = input("Ingresa la cantidad de numeros\n:")
-
-try: 
-  num = int(num)
-except: print("only int numbers"); quit()
-if num < 0 or num>50: print("Numero demasiado grande. Empieza de nuevo"); quit()
-
-
-
-for i in range(1,num+1): arr.append(val_ai(input(f'Ingresa el numero para la posicion {i}\n:' )))
-
-
+num = val(input("Ingresa la cantidad de numeros\n:"), 1)
+#
+for i in range(1,num+1): arr.append(val(input(f'Ingresa el numero para la posicion {i}\n:' ),2))
+#
 for i in arr: print(i)
   
     
