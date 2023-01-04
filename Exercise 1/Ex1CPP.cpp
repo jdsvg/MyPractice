67d4197494bb46de338306600a2c2b2260676981
John Villamil <52754373+jdsvg@users.noreply.github.com>
2023-01-04 02:27:01 +0000

    Continuing 03012023

---
+++ Exercise 1/Ex1CPP.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int num;
+	cin >> num;    //Reading input from STDIN
+	cout << "Input number is " << num << endl;	// Writing output to STDOUT
+}
--- Exercise 1/Ex1Py.py
+++ Exercise 1/Ex1Py.py
@@ -1,2 +1,23 @@
-"""
-"""
+def val_ai(num):
+  try: num = int(num)
+  except: print("only int numbers"); quit()
+  if num < 0 or num>1000: print("Numero demasiado grande. Empieza de nuevo"); quit()
+  return num
+
+
+arr = []
+num = input("Ingresa la cantidad de numeros\n:")
+
+try: 
+  num = int(num)
+except: print("only int numbers"); quit()
+if num < 0 or num>50: print("Numero demasiado grande. Empieza de nuevo"); quit()
+
+
+
+for i in range(1,num+1): arr.append(val_ai(input(f'Ingresa el numero para la posicion {i}\n:' )))
+
+
+for i in arr: print(i)
+  
+    
