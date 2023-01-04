96bc8a86445d46a929dbe5d42233f474f3130c22
John Villamil <52754373+jdsvg@users.noreply.github.com>
2023-01-04 03:46:42 +0000

    Continuing 03012023

--- Exercise 1/Ex1Py.py
+++ Exercise 1/Ex1Py.py
@@ -12,6 +12,4 @@ num = val(input("Enter the amount of numbers\n:"), 1)
 for i in range(1,num+1): arr.append(val(input(f'Enter the number for {i} position \n:' ),2))
 #
 for i in arr: print(i)
-
-
-
+a= 5
