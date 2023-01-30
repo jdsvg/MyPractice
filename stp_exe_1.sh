b252dd29ac3f9e93af0b382e509079c82672da3d
John Villamil <52754373+jdsvg@users.noreply.github.com>
2023-01-30 01:25:14 +0000

    Continuing Challenge

--- stp_exe_1.sh
+++ stp_exe_1.sh
@@ -1,10 +1,17 @@
 #!/bin/sh
 echo "Enter your option"
+echo "1. Python"
+echo "2. Python_Test"
+echo "3. C++"
 read pass
 if [ $pass -eq 1 ]
 then
   python 'Exercise 1'/Ex1Py.py
   # python 'Exercise 1'/Prueba.py
+elif [ $pass -eq 2 ] 
+then
+  python 'Exercise 1'/Prueba.py
+  #echo "Choose a corre" 
 else
-  echo "Choose a corre" 
+  echo "Choose a corret option" 
 fi
